f12a615286f953586a599221bdf662ad0bc79325
= <=>
2023-08-10 19:49:32 +0900

    edits

--- lib/main.dart
+++ lib/main.dart
@@ -19,47 +19,45 @@ class MyApp extends StatelessWidget {
       title: 'Museum App',
       theme: ThemeData(primarySwatch: Colors.cyan,),
       darkTheme: ThemeData.dark(),
-        home: MyHomePage(),
+        home: LoginPage(),
       );
   }
 }
 
 
-// class LoginPage extends StatelessWidget {
-  
-//   @override
-  
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Firebase Authentication'),
-//       ),
-//       body: FutureBuilder(
-//         future: _initializeFirebase(),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.done) {
-//             return Column(
-//               children: [
-//                 Text('Login'),
-//               ],
-//             );
-//           }
-//           return Center(
-//             child: CircularProgressIndicator(),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-
+class LoginPage extends StatelessWidget {
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Authentication'),
+      ),
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                Text('Login'),
+              ],
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
 
 
 class  MyHomePage extends StatelessWidget {
   @override
-
-
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
