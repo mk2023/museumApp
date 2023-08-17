295f5a32175b0fe88eb5637e959db73a9614ab5c
= <=>
2023-08-17 18:29:39 +0900

    final

--- lib/main.dart
+++ lib/main.dart
@@ -26,35 +26,35 @@ class MyApp extends StatelessWidget {
 }
 
 
-// class LoginPage extends StatelessWidget {
-//   Future<FirebaseApp> _initializeFirebase() async {
-//     FirebaseApp firebaseApp = await Firebase.initializeApp();
-//     return firebaseApp;
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Firebase Authentication'),
-//       ),
-//       body: FutureBuilder(
-//         future: _initializeFirebase(),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.done) {
-//             return Column(
-//               children: [
-//                 Text('Login'),
-//               ],
-//             );
-//           }
-//           return Center(
-//             child: CircularProgressIndicator(),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
+class LoginPage extends StatelessWidget {
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Authentication'),
+      ),
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                Text('Login'),
+              ],
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
 
 
 class  MyHomePage extends StatelessWidget {
